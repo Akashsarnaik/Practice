0c05e363eee6418470f93c22b24cc42f158b3aee
Akashsarnaik <115389325+Akashsarnaik@users.noreply.github.com>
2024-01-26 14:17:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 def addtion(a,b,c):
     return a+b+c
 
-<<<<<<< HEAD
-def sub(a,b,e):
-    return a-b-e
-=======
+
 def sub(a,b,c):
     return a-b-c
->>>>>>> af52c933b47c447d209b89404f901a1e6df73c4d
 
 
 def div(a,b):
